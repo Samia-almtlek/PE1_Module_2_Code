ef8abddb1518c532775c40d387557ae7079b647c
Samia-almtlek <146224174+Samia-almtlek@users.noreply.github.com>
2024-01-18 17:31:20 +0100

    Update M1.mjs

--- M1.mjs
+++ M1.mjs
@@ -1,28 +1,34 @@
 import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
-let red=' Red Hot Chili Peppers ';
-let ac=' AC/DC ';
-let net=' Netsky ';
-let de=' De Strangers ';
 
-console.log('Kies een artiest: \n' + ' 1. ' + red + '\n' + ' 2. ' + ac + '\n' + ' 3. ' + net + '\n' + ' 4. ' + de );
+console.log('Kies een artiest: \n 1. Red Hot Chili Peppers\n 2. AC/DC\n 3. Netsky\n 4. De Strangers')
+let keuze=parseFloat(await userInput.question('Geef je keuze in: '));
+let resultaat;
+let songTitel;
+switch (keuze) {
+    case 1:
+        resultaat='Red Hot Chili Peppers';
+        songTitel = 'Californication';
+        break;
+        case 2:
+        resultaat='AC/DC';
+        songTitel = 'Highway to Hell';
+        break;
+        case 3:
+        resultaat='Netsky';   
+        songTitel = 'Memory Lane';
+        break; 
+        case 4:
+        resultaat='De Strangers';
+        songTitel = 'Mijnen blauwe geschelpte';
+        break;
 
-let kies = await userInput.question('Geef je keuze in : ');
-switch(kies){
-    case '1':
-        kies =red ;
+    default:
+console.log('Ongdeldige song')
         break;
-        case '2':
-        kies = ac ;
-        break;
-        case '3':
-        kies = net;
-        break;
-        case '4':
-        kies = de ;
-        break;
+  
 }
-console.log(' Je koos voor ' + '"' + kies +  '"' ); 
+console.log('Je koos voor "' +resultaat +'" Ze zijn de artist achter de hit: ' + songTitel)
 
-process.exit();
+process.exit()
